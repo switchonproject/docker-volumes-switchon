7c4e3a5f0236757a3e5ba726e053691352dadf0c
p-a-s-c-a-l <pascal@cismet.de>
2017-01-24 11:41:39 +0100

    #1 update compose files and run scripts for cids-distribution-switchon release v4.0

--- cids-distribution-switchon/cidsIntegrationBase/run.sh
+++ cids-distribution-switchon/cidsIntegrationBase/run.sh
@@ -8,5 +8,5 @@ docker volume rm cidsdistributionswitchon_cids-integration-base
 docker run -it -p 5436:5432 --name cidsdistributionswitchon_cids-integration-base \
     -v cidsdistributionswitchon_cids-integration-base:/cidsIntegrationBase/pg_data \
     -v ~/switchon-docker-volumes/cids-distribution-switchon/cidsIntegrationBase/import/:/import/cidsIntegrationBase/ \
-    cismet/cids-integration-base:latest \
+    cismet/cids-integration-base:postgres-9.6.1-2.0 \
     start import
--- switchon/cids-integration-base/run.sh
+++ switchon/cids-integration-base/run.sh
@@ -8,5 +8,5 @@ docker volume rm switchon_cids-integration-base
 docker run -it -p 5434:5432 --name switchon_cids-integration-base \
     -v switchon_cids-integration-base:/cidsIntegrationBase/pg_data \
     -v ~/switchon-docker-volumes/switchon/cids-integration-base/import/:/import/cidsIntegrationBase/ \
-    cismet/cids-integration-base:latest \
+    cismet/cids-integration-base:postgres-9.6.1-2.0 \
     start import
